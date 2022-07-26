6939f0d9201be137b92d096984e0b7958d25c385
Yevgeniy Starinets <tsn.ipad@gmail.com>
2022-07-26 12:32:31 -0400

    - add `tank` direction - add `tank` animation

--- src/tank.js
+++ src/tank.js
@@ -1,8 +1,32 @@
 export default class Tank {
   activeKeys = new Set();
+  direction = 'top';
 
   x = 20;
   y = 20;
+
+  frameSize = 16;
+  animationFrame = 1;
+  frames = {
+    top: [
+      [0, 0],
+      [1, 0],
+    ],
+    left: [
+      [2, 0],
+      [3, 0],
+    ],
+    bottom: [
+      [4, 0],
+      [5, 0],
+    ],
+    right: [
+      [6, 0],
+      [7, 0],
+    ],
+  };
+
+  _changeAnimationFrame = () => (this.animationFrame ^= 1);
 
   moveUp = () => (this.activeKeys.up = true);
   moveDown = () => (this.activeKeys.down = true);
@@ -14,22 +38,28 @@ export default class Tank {
   stopRight = () => (this.activeKeys.right = false);
   fire = () => console.log('fire');
 
+  getFrame = () => [
+    this.frames[this.direction][this.animationFrame][0] * this.frameSize,
+    this.frames[this.direction][this.animationFrame][1] * this.frameSize,
+  ];
+
   update() {
     if (this.activeKeys.up) {
       this.y -= 1;
-      return;
-    }
-    if (this.activeKeys.down) {
+      this.direction = 'top';
+      this._changeAnimationFrame();
+    } else if (this.activeKeys.down) {
       this.y += 1;
-      return;
-    }
-    if (this.activeKeys.left) {
+      this.direction = 'bottom';
+      this._changeAnimationFrame();
+    } else if (this.activeKeys.left) {
       this.x -= 1;
-      return;
-    }
-    if (this.activeKeys.right) {
+      this.direction = 'left';
+      this._changeAnimationFrame();
+    } else if (this.activeKeys.right) {
       this.x += 1;
-      return;
+      this.direction = 'right';
+      this._changeAnimationFrame();
     }
   }
 }
